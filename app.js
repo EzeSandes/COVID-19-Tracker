0e88773a9d30a51a53938fa6ad1a6955ab7ae4d6
Eze Sandes <jezesandes@gmail.com>
2021-06-23 15:27:17 -0300

    User country choice function completed

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const todayDeathsNum = document.getElementById('today-deaths-num');
 const totalCasesNum = document.getElementById('total-cases-num');
 const totalDeathsNum = document.getElementById('total-deaths-num');
 const totalRecoveredNum = document.getElementById('recovered-num');
-
+const countryBtn = document.querySelector('.country__btn');
+const countryList = document.getElementById('list-countries');
 
 const getJSON = function (url, errorMsg = `Something went wrong`) {
    return fetch(url).then(response => {
@@ -21,14 +22,13 @@ const numberWithCommas = function (x) {
    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
-/******************  Get visitor's location *****************/
 
-const renderCOVIDdata = function (country) {
-   getJSON(`https://disease.sh/v3/covid-19/countries/${country}?strict=true`).then(data => {
+const renderCOVIDdata = function (countryIso) {
+   getJSON(`https://disease.sh/v3/covid-19/countries/${countryIso}?strict=true`).then(data => {
 
-      const { cases: totalCases, deaths: totalDeaths, countryInfo, todayCases, todayDeaths, recovered, active } = data;
+      const { cases: totalCases, deaths: totalDeaths, countryInfo, todayCases, todayDeaths, recovered, active, country: countryName } = data;
 
-      countryUser.innerText = country;
+      countryUser.innerText = countryName;
       countryFlag.src = `${countryInfo.flag}`;
       activeCasesNum.innerText = numberWithCommas(active);
       todayCasesNum.innerText = numberWithCommas(todayCases);
@@ -40,17 +40,20 @@ const renderCOVIDdata = function (country) {
 };
 
 
+/******************  Get visitor's location *****************/
+////////////////////////
+
 const getPosition = function () {
    return new Promise(function (resolve, reject) {
       navigator.geolocation.getCurrentPosition(resolve, reject);
    });
 };
 
-const getCountryUser = function () {
+const getUserCountry = function () {
    getPosition().then(pos => {
       const { latitude: lat, longitude: long } = pos.coords;
 
-      /*** OLD API: It doesn't support many request per seconds. ** */
+      // OLD API: It doesn't support many request per seconds.
       // return fetch(`https://geocode.xyz/${lat},${long}?geoit=json`);
 
       return fetch(`https://nominatim.geocoding.ai/reverse.php?lat=${lat}&lon=${long}&format=jsonv2`);
@@ -65,11 +68,25 @@ const getCountryUser = function () {
    }).catch(error => console.error(`${error.message} !!`));
 };
 
-/****** Render data of User's country****/
-// getCountryUser();
+const getCountry = function () {
+   const country = countryList.value;
+
+   // return ISO-3166-1: Alpha-2 Codes
+   return country.split(',').reverse()[0].slice(1, 3);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+/****** FUNCTION ONLY TO KNOW USER COUNTRY **** */
+getUserCountry();
+
+/****** Render data of the chosen country  ****/
+countryBtn.addEventListener('click', function () {
+   renderCOVIDdata(getCountry());
+});
+
 
 /**************** TESTS by country ***** */
-// (Add another one or Uncomment for what you want and comment 'getCountryUser()')
+// (Add another one or Uncomment for what you want and comment 'getUserCountry()')
 
 // renderCOVIDdata('Germany');
 // renderCOVIDdata('Argentina');
